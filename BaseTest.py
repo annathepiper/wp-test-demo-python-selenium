9a1fd6b9637689c2dd9d5973d133a4159e8a0343
Angela Korra'ti <annathepiper@fastmail.com>
2019-02-11 11:15:01 -0800

    Fixing naming convention issues w/ docstrings, variables, function names

--- BaseTest.py
+++ BaseTest.py
@@ -5,21 +5,29 @@ import WPTestLib
 
 # BaseTest
 # Written by Angela Korra'ti
-# Last updated 2/7/2019
+# Last updated 2/11/2019
 #
 # This is the base test class for the other ones in the suite.
 
+
 class BaseTest(unittest.TestCase):
     driver = None
-    wpLib = None
+    wp_lib = None
 
     def setUp(self):
-        self.wpLib = WPTestLib.WPTestLib()
+        """
+        Global setup function for all test classes that are children of BaseTest
+        """
+        self.wp_lib = WPTestLib.WPTestLib()
         caps = {'browserName': os.getenv('BROWSER', 'chrome')}
         self.driver = webdriver.Remote(
-            command_executor=self.wpLib.seleniumHost,
+            command_executor=self.wp_lib.selenium_host,
             desired_capabilities=caps)
 
     def tearDown(self):
+        """
+        Global teardown function for all test classes that are children of BaseTest
+        :return:
+        """
         self.driver.close()
         self.driver.quit()
