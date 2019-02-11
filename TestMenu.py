65d153c337c866e915540aff41e4b5cdc39f450e
Angela Korra'ti <annathepiper@fastmail.com>
2019-02-11 11:32:06 -0800

    Fixing assorted naming convention issues, also adding Blog test cases

--- TestMenu.py
+++ TestMenu.py
@@ -3,106 +3,136 @@ import WPHomepage
 
 # TestMenu
 # Written by Angela Korra'ti
-# Last updated 2/8/2019
+# Last updated 2/11/2019
 #
 # This class conducts tests against the main menu of my test Wordpress site.
 
+
 class TestMenu(BaseTest):
-    wpHomepage = None
-    wpMenu = None
+    wp_homepage = None
+    wp_menu = None
 
     def setUp(self):
-        '''Do setup for the test cases.'''
+        """
+        Do setup for the test cases.
+        """
         super().setUp()
-        self.driver.get(self.wpLib.wpBaseUri)
-        self.wpHomepage = WPHomepage.WPHomepage(self.driver, self.wpLib)
-        self.wpMenu = self.wpHomepage.wpMenu
+        self.driver.get(self.wp_lib.wp_base_uri)
+        self.wp_homepage = WPHomepage.WPHomepage(self.driver, self.wp_lib)
+        self.wp_menu = self.wp_homepage.wp_menu
 
     def TestMenuExistsVisible(self):
-        '''
+        """
         Verify that the main menu is present and visible
-        '''
-        menuElement = self.wpMenu.menuElement()
-        assert menuElement is not None
-        assert menuElement.is_displayed()
+        """
+        menu_element = self.wp_menu.menu_element
+        assert menu_element is not None
+        assert menu_element.is_displayed()
 
     def TestHomeMenuExistsVisible(self):
-        '''
-        Verify that the Home menu is present and visible
-        '''
-        homeMenuElement = self.wpMenu.homeMenuElement()
-        assert homeMenuElement is not None
-        assert homeMenuElement.is_displayed()
+        """
+        Verify that the Home menu is present and visible        
+        """
+        home_menu_element = self.wp_menu.home_menu_element
+        assert home_menu_element is not None
+        assert home_menu_element.is_displayed()
 
     def TestHomeMenuText(self):
-        '''
+        """
         Verify the Home menu text is correct
-        '''
-        assert self.wpMenu.homeMenuText == self.wpLib.menuHome['text']
+        """
+        assert self.wp_menu.home_menu_text == self.wp_lib.menu_home['text']
 
     def TestHomeMenuLink(self):
-        '''
+        """
         Verify the Home menu link is correct
-        '''
-        assert self.wpMenu.homeMenuLink == self.wpLib.menuHome['link']
+        """
+        assert self.wp_menu.home_menu_link == self.wp_lib.menu_home['link']
 
     def TestHomeMenuLinkClick(self):
-        '''
-        Verify clicking the Home menu link lands you on the homepage
-        '''
-        self.wpMenu.homeMenuElement().click()
-        assert self.driver.current_url == self.wpMenu.homeMenuLink
+        """
+        Verify clicking the Home menu link lands you on the homepage        
+        """
+        self.wp_menu.home_menu_element.click()
+        assert self.driver.current_url == self.wp_menu.home_menu_link
 
     def TestAboutMenuExistsVisible(self):
-        '''
+        """
         Verify that the About menu is present and visible
-        '''
-        aboutMenuElement = self.wpMenu.aboutMenuElement()
-        assert aboutMenuElement is not None
-        assert aboutMenuElement.is_displayed()
+        """
+        about_menu_element = self.wp_menu.about_menu_element
+        assert about_menu_element is not None
+        assert about_menu_element.is_displayed()
 
     def TestAboutMenuText(self):
-        '''
+        """
         Verify the About menu text is correct
-        '''
-        assert self.wpMenu.aboutMenuText == self.wpLib.menuAbout['text']
+        """
+        assert self.wp_menu.about_menu_text == self.wp_lib.menu_about['text']
 
     def TestAboutMenuLink(self):
-        '''
+        """
         Verify the About menu link is correct
-        '''
-        assert self.wpMenu.aboutMenuLink == self.wpLib.menuAbout['link']
+        """
+        assert self.wp_menu.about_menu_link == self.wp_lib.menu_about['link']
 
     def TestAboutMenuLinkClick(self):
-        '''
+        """
         Verify clicking the About menu link lands you on the About page
-        '''
-        self.wpMenu.aboutMenuElement().click()
-        assert self.driver.current_url == self.wpMenu.aboutMenuLink
+        """
+        self.wp_menu.about_menu_element.click()
+        assert self.driver.current_url == self.wp_menu.about_menu_link
 
     def TestBooksMenuExistsVisible(self):
-        '''
+        """
         Verify that the Books menu is present and visible
-        '''
-        booksMenuElement = self.wpMenu.booksMenuElement()
-        assert booksMenuElement is not None
-        assert booksMenuElement.is_displayed()
+        """
+        books_menu_element = self.wp_menu.books_menu_element
+        assert books_menu_element is not None
+        assert books_menu_element.is_displayed()
 
     def TestBooksMenuText(self):
-        '''
+        """
         Verify the Books menu text is correct
-        '''
-        assert self.wpMenu.booksMenuText == self.wpLib.menuBooks['text']
+        """
+        assert self.wp_menu.books_menu_text == self.wp_lib.menu_books['text']
 
     def TestBooksMenuLink(self):
-        '''
+        """
         Verify the Books menu link is correct
-        '''
-        assert self.wpMenu.booksMenuLink == self.wpLib.menuBooks['link']
+        """
+        assert self.wp_menu.books_menu_link == self.wp_lib.menu_books['link']
 
     def TestBooksMenuLinkClick(self):
-        '''
+        """
         Verify clicking the Books menu link lands you on the Books page
-        '''
-        self.wpMenu.booksMenuElement().click()
-        assert self.driver.current_url == self.wpMenu.booksMenuLink
+        """
+        self.wp_menu.books_menu_element.click()
+        assert self.driver.current_url == self.wp_menu.books_menu_link
+
+    def TestBlogMenuExistsVisible(self):
+        """
+        Verify that the Blog menu is present and visible
+        """
+        blog_menu_element = self.wp_menu.blog_menu_element
+        assert blog_menu_element is not None
+        assert blog_menu_element.is_displayed()
+
+    def TestBlogMenuText(self):
+        """
+        Verify the Blog menu text is correct
+        """
+        assert self.wp_menu.blog_menu_text == self.wp_lib.menu_blog['text']
+
+    def TestBlogMenuLink(self):
+        """
+        Verify the Blog menu link is correct
+        """
+        assert self.wp_menu.blog_menu_link == self.wp_lib.menu_blog['link']
+
+    def TestBlogMenuLinkClick(self):
+        """
+        Verify clicking the Blog menu link lands you on the Blog page
+        """
+        self.wp_menu.blog_menu_element.click()
+        assert self.driver.current_url == self.wp_menu.blog_menu_link
