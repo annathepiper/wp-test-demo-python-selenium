60845d534ed6fd05b0fa9256a535cea5e88b60ed
Angela Korra'ti <annathepiper@fastmail.com>
2019-02-22 16:22:58 -0800

    Remaining sidebar test cases ported over from Java project

--- TestSidebar.py
+++ TestSidebar.py
@@ -3,7 +3,7 @@ import WPHomepage
 
 # TestSidebar
 # Written by Angela Korra'ti
-# Last updated 2/21/2019
+# Last updated 2/22/2019
 #
 # This class conducts tests against the sidebar of my test Wordpress site.
 
@@ -70,8 +70,7 @@ class TestSidebar(BaseTest):
         """
         Verify that the sidebar recent posts widget has the correct title text
         """
-        recent_posts_title = self.wp_sidebar.sidebar_recent_posts_title_element
-        assert recent_posts_title.text == self.wp_lib.sidebar_recent_posts_title_text
+        assert self.wp_sidebar.sidebar_recent_posts_title_text == self.wp_lib.sidebar_recent_posts_title_text
 
     def TestRecentPostsListExistsVisible(self):
         """
@@ -108,8 +107,7 @@ class TestSidebar(BaseTest):
         """
         Verify that the sidebar recent comments widget has the correct title text
         """
-        recent_comments_title = self.wp_sidebar.sidebar_recent_comments_title_element
-        assert recent_comments_title.text == self.wp_lib.sidebar_recent_comments_title_text
+        assert self.wp_sidebar.sidebar_recent_comments_title_text == self.wp_lib.sidebar_recent_comments_title_text
 
     def TestRecentCommentsListExistsVisible(self):
         """
@@ -125,3 +123,114 @@ class TestSidebar(BaseTest):
         """
         recent_comments_list_items = self.wp_sidebar.sidebar_recent_comments_list_items_elements
         assert len(recent_comments_list_items) >= 1
+
+    def TestArchivesWidgetExistsVisible(self):
+        """
+        Verify that the sidebar archives widget is present and visible
+        """
+        archives_widget = self.wp_sidebar.sidebar_archives_element
+        assert archives_widget is not None
+        assert archives_widget.is_displayed()
+
+    def TestArchivesTitleExistsVisible(self):
+        """
+        Verify that the sidebar archives widget has a title
+        """
+        archives_title = self.wp_sidebar.sidebar_archives_title_element
+        assert archives_title is not None
+        assert archives_title.is_displayed()
+
+    def TestArchivesTitleText(self):
+        """
+        Verify that the sidebar archives widget has the correct title text
+        """
+        assert self.wp_sidebar.sidebar_archives_title_text == self.wp_lib.sidebar_archives_title_text
+
+    def TestArchivesListExistsVisible(self):
+        """
+        Verify that the sidebar archives widget has a list of archives
+        """
+        archives_list = self.wp_sidebar.sidebar_archives_list_element
+        assert archives_list is not None
+        assert archives_list.is_displayed()
+
+    def TestArchivesListItemsCount(self):
+        """
+        Verify that the sidebar archives widget contains at least one archive
+        """
+        archives_list_items = self.wp_sidebar.sidebar_archives_list_items_elements
+        assert len(archives_list_items) >= 1
+
+    def TestCategoriesWidgetExistsVisible(self):
+        """
+        Verify that the sidebar categories widget is present and visible
+        """
+        categories_widget = self.wp_sidebar.sidebar_categories_element
+        assert categories_widget is not None
+        assert categories_widget.is_displayed()
+
+    def TestCategoriesTitleExistsVisible(self):
+        """
+        Verify that the sidebar categories widget has a title
+        """
+        categories_title = self.wp_sidebar.sidebar_categories_title_element
+        assert categories_title is not None
+        assert categories_title.is_displayed()
+
+    def TestCategoriesTitleText(self):
+        """
+        Verify that the sidebar categories widget has the correct title text
+        """
+        assert self.wp_sidebar.sidebar_categories_title_text == self.wp_lib.sidebar_categories_title_text
+
+    def TestCategoriesListExistsVisible(self):
+        """
+        Verify that the sidebar categories widget has a list of categories
+        """
+        categories_list = self.wp_sidebar.sidebar_categories_list_element
+        assert categories_list is not None
+        assert categories_list.is_displayed()
+
+    def TestCategoriesListItemsCount(self):
+        """
+        Verify that the sidebar categories widget contains at least one category
+        """
+        categories_list_items = self.wp_sidebar.sidebar_categories_list_items_elements
+        assert len(categories_list_items) >= 1
+
+    def TestMetaWidgetExistsVisible(self):
+        """
+        Verify that the sidebar meta widget is present and visible
+        """
+        meta_widget = self.wp_sidebar.sidebar_meta_element
+        assert meta_widget is not None
+        assert meta_widget.is_displayed()
+
+    def TestMetaTitleExistsVisible(self):
+        """
+        Verify that the sidebar categories meta has a title
+        """
+        meta_title = self.wp_sidebar.sidebar_meta_title_element
+        assert meta_title is not None
+        assert meta_title.is_displayed()
+
+    def TestMetaTitleText(self):
+        """
+        Verify that the sidebar meta widget has the correct title text
+        """
+        assert self.wp_sidebar.sidebar_meta_title_text == self.wp_lib.sidebar_meta_title_text
+
+    def TestMetaListExistsVisible(self):
+        """
+        Verify that the sidebar meta widget has a list of categories
+        """
+        meta_list = self.wp_sidebar.sidebar_meta_list_element
+        assert meta_list is not None
+        assert meta_list.is_displayed()
+
+    def TestMetaListItemsCount(self):
+        """
+        Verify that the sidebar meta widget contains the correct number of meta links
+        """
+        meta_list_items = self.wp_sidebar.sidebar_meta_list_items_elements
+        assert len(meta_list_items) == 4
