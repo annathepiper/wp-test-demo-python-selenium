43c761886f3e01d1e0a96ec549db68514339168c
Angela Korra'ti <annathepiper@fastmail.com>
2019-04-24 16:35:05 -0700

    Post Footer test class
    Test class for verifying the footer against a post. Inherits from TestFooter parent class.

--- TestPostFooter.py
+++ TestPostFooter.py
@@ -1,6 +1,5 @@
-from TestFooter import TestFooter
-import WPHomepage
-from selenium.webdriver.common.action_chains import ActionChains
+from TestHomepageFooter import TestFooter
+import WPPost
 
 # TestFooter
 # Written by Angela Korra'ti
@@ -11,18 +10,119 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 class TestPostFooter(TestFooter):
     wp_footer = None
-    ac = None
 
     def setUp(self):
         """
         Do setup for the test cases.
         """
         super().setUp()
-        self.driver.get(self.wp_lib.wp_post_uri)
-        self.wp_footer = WPHomepage.WPHomepage(self.driver, self.wp_lib).wp_footer
-        self.ac = ActionChains(self.driver)
+        self.driver.get(self.wp_lib.wp_base_uri)
+        self.wp_footer = WPPost.WPPost(self.driver, self.wp_lib).wp_footer
 
-    def TestFooterSiteTitle(self):
+    def test_site_title(self):
         """
         Verify the footer site title element is present and visible on a post
         """
+        self.verify_site_title(self.wp_footer)
+
+    def test_site_title_text(self):
+        """
+        Verify that a post footer site title element has the correct text
+        """
+        self.verify_site_title_text(self.wp_footer)
+
+    def test_site_title_link_click(self):
+        """
+        Verify clicking on a post footer site title link takes you to the homepage
+        """
+        self.verify_site_title_link_click(self.wp_footer)
+
+    def test_wordpress(self):
+        """
+        Verify a post footer Wordpress element is present and visible
+        """
+        self.verify_wordpress(self.wp_footer)
+
+    def test_wordpress_text(self):
+        """
+        Verify that a post footer Wordpress element has the correct text
+        """
+        self.verify_wordpress_text(self.wp_footer)
+
+    def test_wordpress_link_click(self):
+        """
+        Verify that clicking on a post footer Wordpress link lands on correct page (wordpress.org)
+        """
+        self.verify_wordpress_link_click(self.wp_footer)
+
+    def test_social_facebook(self):
+        """
+        Verify a post footer Facebook element is present and visible
+        """
+        self.verify_social_facebook(self.wp_footer)
+
+    def test_social_facebook_text(self):
+        """
+        Verify that a post footer Facebook element has the correct text
+        """
+        self.verify_social_facebook_text(self.wp_footer)
+
+    def test_social_facebook_link_click(self):
+        """
+        Verify that clicking a post footer Facebook link lands on correct page
+        """
+        self.verify_social_facebook_link_click(self.wp_footer)
+
+    def test_social_twitter(self):
+        """
+        Verify a post footer Twitter element is present and visible
+        """
+        self.verify_social_twitter(self.wp_footer)
+
+    def test_social_twitter_text(self):
+        """
+        Verify that a post footer Twitter element has the correct text
+        """
+        self.verify_social_twitter_text(self.wp_footer)
+
+    def test_social_twitter_link_click(self):
+        """
+        Verify that clicking a post footer Twitter link lands on correct page
+        """
+        self.verify_social_twitter_link_click(self.wp_footer)
+
+    def test_social_github(self):
+        """
+        Verify a post footer Github element is present and visible
+        """
+        self.verify_social_github(self.wp_footer)
+
+    def test_social_github_text(self):
+        """
+        Verify that a post footer Github element has the correct text
+        """
+        self.verify_social_github_text(self.wp_footer)
+
+    def test_social_github_link_click(self):
+        """
+        Verify that clicking a post footer Github link lands on correct page
+        """
+        self.verify_social_github_link_click(self.wp_footer)
+
+    def test_social_linkedin(self):
+        """
+        Verify a post footer LinkedIn element is present and visible
+        """
+        self.verify_social_linkedin(self.wp_footer)
+
+    def test_social_linkedin_text(self):
+        """
+        Verify that a post footer LinkedIn element has the correct text
+        """
+        self.verify_social_linkedin_text(self.wp_footer)
+
+    def test_social_linkedin_link_click(self):
+        """
+        Verify that clicking a post footer LinkedIn link lands on correct page
+        """
+        self.verify_social_linkedin_link_click(self.wp_footer)
