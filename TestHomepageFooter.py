09fcba485fc3df66f0146e31e12bb38ec7e950b8
Angela Korra'ti <annathepiper@fastmail.com>
2019-05-09 15:22:42 -0700

    Refactor of this class
    Taking advantage of the refactor of the parent footer class changes.

--- TestHomepageFooter.py
+++ TestHomepageFooter.py
@@ -9,7 +9,6 @@ import WPHomepage
 
 
 class TestHomepageFooter(TestFooter):
-    wp_footer = None
 
     def setUp(self):
         """
@@ -17,112 +16,40 @@ class TestHomepageFooter(TestFooter):
         """
         super().setUp()
         self.driver.get(self.wp_lib.wp_base_uri)
-        self.wp_footer = WPHomepage.WPHomepage(self.driver, self.wp_lib).wp_footer
+        super().set_wp_footer(WPHomepage.WPHomepage(self.driver, self.wp_lib).wp_footer)
 
     def test_site_title(self):
         """
-        Verify the footer site title element is present and visible on the homepage
+        Verify the footer site title on the homepage
         """
-        self.verify_site_title(self.wp_footer)
-
-    def test_site_title_text(self):
-        """
-        Verify that the homepage footer site title element has the correct text
-        """
-        self.verify_site_title_text(self.wp_footer)
-
-    def test_site_title_link_click(self):
-        """
-        Verify clicking on the homepage footer site title link takes you to the homepage
-        """
-        self.verify_site_title_link_click(self.wp_footer)
+        self.verify_site_title()
 
     def test_wordpress(self):
         """
-        Verify the homepage footer Wordpress element is present and visible
+        Verify the footer Wordpress element on the homepage
         """
-        self.verify_wordpress(self.wp_footer)
-
-    def test_wordpress_text(self):
-        """
-        Verify that the homepage footer Wordpress element has the correct text
-        """
-        self.verify_wordpress_text(self.wp_footer)
-
-    def test_wordpress_link_click(self):
-        """
-        Verify that clicking on the homepage footer Wordpress link lands on correct page (wordpress.org)
-        """
-        self.verify_wordpress_link_click(self.wp_footer)
+        self.verify_wordpress()
 
     def test_social_facebook(self):
         """
-        Verify the homepage footer Facebook element is present and visible
+        Verify the footer Facebook element on the homepage
         """
-        self.verify_social_facebook(self.wp_footer)
-
-    def test_social_facebook_text(self):
-        """
-        Verify that the homepage footer Facebook element has the correct text
-        """
-        self.verify_social_facebook_text(self.wp_footer)
-
-    def test_social_facebook_link_click(self):
-        """
-        Verify that clicking the homepage footer Facebook link lands on correct page
-        """
-        self.verify_social_facebook_link_click(self.wp_footer)
+        self.verify_social_facebook()
 
     def test_social_twitter(self):
         """
-        Verify the homepage footer Twitter element is present and visible
+        Verify the footer Twitter element on the homepage
         """
-        self.verify_social_twitter(self.wp_footer)
-
-    def test_social_twitter_text(self):
-        """
-        Verify that the homepage footer Twitter element has the correct text
-        """
-        self.verify_social_twitter_text(self.wp_footer)
-
-    def test_social_twitter_link_click(self):
-        """
-        Verify that clicking the homepage footer Twitter link lands on correct page
-        """
-        self.verify_social_twitter_link_click(self.wp_footer)
+        self.verify_social_twitter()
 
     def test_social_github(self):
         """
-        Verify the homepage footer Github element is present and visible
+        Verify the footer Github element on the homepage
         """
-        self.verify_social_github(self.wp_footer)
-
-    def test_social_github_text(self):
-        """
-        Verify that the homepage footer Github element has the correct text
-        """
-        self.verify_social_github_text(self.wp_footer)
-
-    def test_social_github_link_click(self):
-        """
-        Verify that clicking the homepage footer Github link lands on correct page
-        """
-        self.verify_social_github_link_click(self.wp_footer)
+        self.verify_social_github()
 
     def test_social_linkedin(self):
         """
-        Verify the homepage footer LinkedIn element is present and visible
+        Verify the footer LinkedIn element on the homepage
         """
-        self.verify_social_linkedin(self.wp_footer)
-
-    def test_social_linkedin_text(self):
-        """
-        Verify that the homepage footer LinkedIn element has the correct text
-        """
-        self.verify_social_linkedin_text(self.wp_footer)
-
-    def test_social_linkedin_link_click(self):
-        """
-        Verify that clicking the homepage footer LinkedIn link lands on correct page
-        """
-        self.verify_social_linkedin_link_click(self.wp_footer)
+        self.verify_social_linkedin()
