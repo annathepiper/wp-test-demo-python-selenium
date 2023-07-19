f6d077169519deb57f7c081f79b46640359f8f86
Angela Korra'ti <annathepiper@fastmail.com>
2023-07-19 12:13:28 -0700

    Rework of this test class to eliminate test_footer out of the class chain

--- test_post_footer.py
+++ test_post_footer.py
@@ -1,54 +1,57 @@
-from test_footer import TestFooter
 import WPPost
+from BaseTest import BaseTest
+
 
 # TestFooter
 # Written by Angela Korra'ti
-# Last updated 7/6/2023
+# Last updated 7/19/2023
 #
-# This class conducts tests against the footer on a post of my test Wordpress site.
+# This class conducts tests against the footer on a post of my test WordPress site.
 
 
-class TestPostFooter(TestFooter):
+class TestPostFooter(BaseTest):
+    wp_footer = None
+
     def setup_method(self):
         """
         Do setup for the test cases.
         """
         super().setup_method()
         self.driver.get(self.wp_lib.wp_post_uri)
-        super().set_wp_footer(WPPost.WPPost(self.driver, self.wp_lib).wp_footer)
+        self.wp_footer = WPPost.WPPost(self.driver, self.wp_lib).wp_footer
 
     def test_site_title(self):
         """
         Verify the footer site title on a post
         """
-        self.verify_site_title()
+        self.wp_footer.verify_site_title()
 
     def test_wordpress(self):
         """
-        Verify the footer Wordpress element on a post
+        Verify the footer WordPress element on a post
         """
-        self.verify_wordpress()
+        self.wp_footer.verify_wordpress()
 
     def test_social_facebook(self):
         """
         Verify the footer Facebook element on a post
         """
-        self.verify_social_facebook()
+        self.wp_footer.verify_social_facebook()
 
     def test_social_mastodon(self):
         """
         Verify the footer Mastodon element on a post
         """
-        self.verify_social_mastodon()
+        self.wp_footer.verify_social_mastodon()
 
     def test_social_github(self):
         """
-        Verify the footer Github element on a post
+        Verify the footer GitHub element on a post
         """
-        self.verify_social_github()
+        self.wp_footer.verify_social_github()
 
     def test_social_linkedin(self):
         """
         Verify the footer LinkedIn element on a post
         """
-        self.verify_social_linkedin()
+        self.wp_footer.verify_social_linkedin()
