f610b63d79050500a74ecb8eb66c618c34c6f0d9
Angela Korra'ti <annathepiper@fastmail.com>
2023-07-19 14:03:05 -0700

    Reworked to eliminate test_sidebar dependency Eliminated the test_sidebar class completely, so now these tests run verifications off their local wp_sidebar object, and inherit directly from BaseTest.

--- test_homepage_sidebar.py
+++ test_homepage_sidebar.py
@@ -1,14 +1,15 @@
-from test_sidebar import TestSidebar
 import WPHomepage
+from BaseTest import BaseTest
 
 # TestHomepageSidebar
 # Written by Angela Korra'ti
-# Last updated 7/6/2023
+# Last updated 7/19/2023
 #
 # This class conducts tests against the menu on the homepage of my test WordPress site.
 
 
-class TestHomepageSidebar(TestSidebar):
+class TestHomepageSidebar(BaseTest):
+    wp_sidebar = None
 
     def setup_method(self):
         """
@@ -16,52 +17,52 @@ class TestHomepageSidebar(TestSidebar):
         """
         super().setup_method()
         self.driver.get(self.wp_lib.wp_base_uri)
-        super().set_wp_sidebar(WPHomepage.WPHomepage(self.driver, self.wp_lib).wp_sidebar)
+        self.wp_sidebar = WPHomepage.WPHomepage(self.driver, self.wp_lib).wp_sidebar
 
     def test_search_widget(self):
         """
         Verify the homepage sidebar search widget is present and visible
         """
-        self.verify_search_widget()
+        self.wp_sidebar.verify_search_widget()
 
     def test_search_input(self):
         """
         Verify the homepage sidebar search input box is present and visible and has the expected text
         """
-        self.verify_search_input()
+        self.wp_sidebar.verify_search_input()
 
     def test_search_button(self):
         """
         Verify the homepage sidebar search button exists and is visible
         """
-        self.verify_search_button()
+        self.wp_sidebar.verify_search_button()
 
     def test_recent_posts(self):
         """
         Verify the homepage recent posts widget is present and visible, has the correct title, and has five posts
         """
-        self.verify_recent_posts()
+        self.wp_sidebar.verify_recent_posts()
 
     def test_recent_comments(self):
         """
         Verify the homepage recent comments widget is present and visible, and has the correct title and comment count
         """
-        self.verify_recent_comments()
+        self.wp_sidebar.verify_recent_comments()
 
     def test_archives(self):
         """
         Verify the homepage archives widget is present and visible, and has the correct title and link count
         """
-        self.verify_archives()
+        self.wp_sidebar.verify_archives()
 
     def test_categories(self):
         """
         Verify the homepage categories widget is present and visible, and has the correct title and link count
         """
-        self.verify_categories()
+        self.wp_sidebar.verify_categories()
 
     def test_meta(self):
         """
         Verify the homepage meta widget is present and visible, and has the correct title and link count
         """
-        self.verify_meta()
+        self.wp_sidebar.verify_meta()
--- test_homepage_sidebar_links.py
+++ test_homepage_sidebar_links.py
@@ -1,14 +1,14 @@
-from test_sidebar_links import TestSidebarLinks
 import WPHomepage
+from BaseTest import BaseTest
 
 # TestHomepageSidebarLinks
 # Written by Angela Korra'ti
-# Last updated 7/6/2023
+# Last updated 7/19/2023
 #
 # This class conducts tests against the sidebar of my test WordPress site.
 
 
-class TestHomepageSidebarLinks(TestSidebarLinks):
+class TestHomepageSidebarLinks(BaseTest):
     wp_sidebar = None
 
     def setup_method(self):
@@ -17,34 +17,34 @@ class TestHomepageSidebarLinks(TestSidebarLinks):
         """
         super().setup_method()
         self.driver.get(self.wp_lib.wp_base_uri)
-        super().set_wp_sidebar(WPHomepage.WPHomepage(self.driver, self.wp_lib).wp_sidebar)
+        self.wp_sidebar = WPHomepage.WPHomepage(self.driver, self.wp_lib).wp_sidebar
 
     def test_recent_posts_link_click(self):
         """
         Verify clicking the first link in the Recent Posts links goes to the expected post from the homepage
         """
-        self.verify_recent_posts_link_click()
+        self.wp_sidebar.verify_recent_posts_link_click()
 
     def test_recent_comments_link_click(self):
         """
         Verify clicking the first link in the Recent Comments links goes to the expected comment from the homepage
         """
-        self.verify_recent_comments_link_click()
+        self.wp_sidebar.verify_recent_comments_link_click()
 
     def test_archives_link_click(self):
         """
         Verify clicking the first link in the Archives links goes to the expected archives page from the homepage
         """
-        self.verify_archives_link_click()
+        self.wp_sidebar.verify_archives_link_click()
 
     def test_categories_link_click(self):
         """
         Verify clicking the first link in the Categories links goes to the expected category page from the homepage
         """
-        self.verify_categories_link_click()
+        self.wp_sidebar.verify_categories_link_click()
 
     def test_meta_login_link_click(self):
         """
         Verify clicking on the first link in the Meta links goes to the login page from the homepage
         """
-        self.verify_meta_login_link_click()
+        self.wp_sidebar.verify_meta_login_link_click()
