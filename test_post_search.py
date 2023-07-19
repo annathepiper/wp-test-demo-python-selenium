ad151d3dd51964af2ac7f19e41664fb1f006d20b
Angela Korra'ti <annathepiper@fastmail.com>
2023-07-19 14:24:27 -0700

    Reworked to remove test_sidebar dependency

--- test_post_search.py
+++ test_post_search.py
@@ -1,14 +1,14 @@
-from test_search import TestSearch
 import WPPost
+from BaseTest import BaseTest
 
 # TestPostSearch
 # Written by Angela Korra'ti
-# Last updated 7/6/2023
+# Last updated 7/19/2023
 #
 # This class conducts search tests against my test WordPress site.
 
 
-class TestPostSearch(TestSearch):
+class TestPostSearch(BaseTest):
     wp_sidebar = None
 
     def setup_method(self):
@@ -23,22 +23,22 @@ class TestPostSearch(TestSearch):
         """
         Verify searching for 'Faerie Blood' and pressing Enter works as expected on the homepage
         """
-        self.verify_search_faerie_blood_enter()
+        self.wp_sidebar.verify_search_faerie_blood_enter()
 
     def test_search_faerie_blood_click(self):
         """
         Verify searching for 'Faerie Blood' and clicking search button works as expected on the homepage
         """
-        self.verify_search_faerie_blood_click()
+        self.wp_sidebar.verify_search_faerie_blood_click()
 
     def test_search_wards_enter(self):
         """
         Verify searching for 'Walk the Wards' and pressing Enter works as expected on the homepage
         """
-        self.verify_search_wards_enter()
+        self.wp_sidebar.verify_search_wards_enter()
 
     def test_search_wards_click(self):
         """
         Verify searching for 'Walk the Wards' and clicking search button works as expected on the homepage
         """
-        self.verify_search_wards_click()
+        self.wp_sidebar.verify_search_wards_click()
