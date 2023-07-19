bb3322223eb4c93c7d0dedfc0b26b6103a360de6
Angela Korra'ti <annathepiper@fastmail.com>
2023-07-19 14:02:22 -0700

    Removed all strings pertaining to the sidebar

--- WPTestLib.py
+++ WPTestLib.py
@@ -1,8 +1,8 @@
 # WPTestLib
 # Written by Angela Korra'ti
-# Last updated 5/10/2019
+# Last updated 7/19/2023
 #
-# This is a helper class to contain various test strings, ids, classes, and xpaths.
+# This is a helper class to contain various test strings, ids, classes, and XPaths.
 
 
 class WPTestLib:
@@ -67,58 +67,3 @@ class WPTestLib:
     submenu_store = {"XPath": "//*[@id='menu-primary']/li[6]/ul/li/a",
                     "text": "Bone Walker: The Free Court of Seattle Soundtrack",
                     "link": "http://wordpress.local/bone-walker-the-free-court-of-seattle-soundtrack/"}
-
-    # Strings pertaining to the sidebar
-    sidebar_search_id = "search-2"
-    sidebar_search_input_xpath = "//*[@id='search-2']/form/label/input"
-    sidebar_search_input_text = "Search \u2026"
-    sidebar_search_button_xpath = "//*[@id='search-2']/form/button"
-    sidebar_recent_posts_id = "recent-posts-2"
-    sidebar_recent_posts_title_xpath = "//*[@id='recent-posts-2']/h2"
-    sidebar_recent_posts_title_text = "RECENT POSTS"
-    sidebar_recent_posts_list_xpath = "//*[@id='recent-posts-2']/*/ul"
-    sidebar_recent_comments_id = "recent-comments-2"
-    sidebar_recent_comments_title_xpath = "//*[@id='recent-comments-2']/h2"
-    sidebar_recent_comments_title_text = "RECENT COMMENTS"
-    sidebar_recent_comments_list_xpath = "//*[@id='recent-comments-2']/*/ul"
-    sidebar_archives_id = "archives-2"
-    sidebar_archives_title_xpath = "//*[@id='archives-2']/h2"
-    sidebar_archives_title_text = "ARCHIVES"
-    sidebar_archives_list_xpath = "//*[@id='archives-2']/*/ul"
-    sidebar_categories_id = "categories-2"
-    sidebar_categories_title_xpath = "//*[@id='categories-2']/h2"
-    sidebar_categories_title_text = "CATEGORIES"
-    sidebar_categories_list_xpath = "//*[@id='categories-2']/*/ul"
-    sidebar_meta_id = "meta-2"
-    sidebar_meta_title_xpath = "//*[@id='meta-2']/h2"
-    sidebar_meta_title_text = "META"
-    sidebar_meta_list_xpath = "//*[@id='meta-2']/*/ul"
-
-    # Strings pertaining to search
-    search_string = "Faerie Blood"
-    search_uri = "/?s=Faerie+Blood"
-    search_results_string = "Search Results for: "
-    search_no_results_string = "Walk the Wards"
-    search_no_results_uri = "/?s=Walk+the+Wards"
-    search_no_results_message = "Nothing Found"
-
-    # Strings pertaining to testing clicking on Recent Posts
-    recent_posts_uri = "/2017/04/17/angelahighland-info-domain-now-active/"
-    recent_posts_title = "angelahighland.info domain now active"
-
-    # Strings pertaining to testing clicking on Recent Comments
-    recent_comments_uri = "/2016/09/12/hello-readers/#comment-"
-    recent_comments_title = "Hello, readers!"
-
-    # Strings pertaining to testing clicking on Archives
-    archives_uri = "/2017/04/"
-    archives_string = "Month: "
-    archives_title = "April 2017"
-
-    # Strings pertaining to testing clicking on Categories
-    categories_uri = "/category/about-me/"
-    categories_string = "Category: "
-    categories_title = "About Me"
-
-    # Strings pertaining to testing clicking on Meta links
-    meta_login_uri = "/wp-login.php"
