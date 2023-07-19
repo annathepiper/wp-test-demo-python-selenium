86da7784c515c291fc832f944b3ab645905cc903
Angela Korra'ti <annathepiper@fastmail.com>
2023-07-19 14:23:57 -0700

    Added verify methods for search

--- WPSidebar.py
+++ WPSidebar.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import WPPost
+from selenium.webdriver.common.keys import Keys
 
 # WPSidebar
 # Written by Angela Korra'ti
@@ -382,7 +383,7 @@ class WPSidebar:
         self.recent_comments_list_elements[0].click()
 
         # 7/7/2023 Looking for MOST of the URI here because the actual comment number changes
-        # depending on when I last set up the test Wordpress database, or if I had to import
+        # depending on when I last set up the test WordPress database, or if I had to import
         # multiple times
         most_of_uri = self.wp_lib.wp_base_uri + self.recent_comments_uri
         assert self.driver.current_url.startswith(most_of_uri)
@@ -423,3 +424,69 @@ class WPSidebar:
         self.meta_list_elements[0].click()
         assert self.driver.current_url == self.wp_lib.wp_base_uri + self.meta_login_uri
         self.driver.back()
+
+    def verify_search_faerie_blood_enter(self):
+        """
+        Verify searching for 'Faerie Blood' and pressing Enter works as expected
+        """
+        # Do the search
+        self.search_input_element.send_keys(self.search_string + Keys.RETURN)
+
+        # Make sure we land on the correct URL
+        assert self.driver.current_url == self.wp_lib.wp_base_uri + self.search_uri
+
+        # Make sure the search string is appropriately reflected in the page title
+        page_title = self.driver.find_element(By.CLASS_NAME, self.wp_lib.page_title_class)
+        assert page_title is not None
+        assert page_title.is_displayed()
+        assert page_title.text == self.search_results_string + self.search_string
+
+    def verify_search_faerie_blood_click(self):
+        """
+        Verify searching for 'Faerie Blood' and clicking search button works as expected
+        """
+        # Do the search
+        self.search_input_element.send_keys(self.search_string)
+        self.search_button_element.click()
+
+        # Make sure we land on the correct URL
+        assert self.driver.current_url == self.wp_lib.wp_base_uri + self.search_uri
+
+        # Make sure the search string is appropriately reflected in the page title
+        page_title = self.driver.find_element(By.CLASS_NAME, self.wp_lib.page_title_class)
+        assert page_title is not None
+        assert page_title.is_displayed()
+        assert page_title.text == self.search_results_string + self.search_string
+
+    def verify_search_wards_enter(self):
+        """
+        Verify searching for 'Walk the Wards' and pressing Enter works as expected
+        """
+        # Do the search
+        self.search_input_element.send_keys(self.search_no_results_string + Keys.RETURN)
+
+        # Make sure we land on the correct URL
+        assert self.driver.current_url == self.wp_lib.wp_base_uri + self.search_no_results_uri
+
+        # Make sure the search string is appropriately reflected in the page title
+        page_title = self.driver.find_element(By.CLASS_NAME, self.wp_lib.page_title_class)
+        assert page_title is not None
+        assert page_title.is_displayed()
+        assert page_title.text == self.search_no_results_message
+
+    def verify_search_wards_click(self):
+        """
+        Verify searching for 'Walk the Wards' and clicking search button works as expected
+        """
+        # Do the search
+        self.search_input_element.send_keys(self.search_no_results_string)
+        self.search_button_element.click()
+
+        # Make sure we land on the correct URL
+        assert self.driver.current_url == self.wp_lib.wp_base_uri + self.search_no_results_uri
+
+        # Make sure the search string is appropriately reflected in the page title
+        page_title = self.driver.find_element(By.CLASS_NAME, self.wp_lib.page_title_class)
+        assert page_title is not None
+        assert page_title.is_displayed()
+        assert page_title.text == self.search_no_results_message
